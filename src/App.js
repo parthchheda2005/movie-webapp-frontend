5c97ef452396afa5a1ab5d5f3d1e585cabc0420f
parthchheda2005 <path.h.chheda@gmail.com>
2024-05-20 22:09:40 +0800

    made the foundation of the app, consisiting of the home page, working search bar and nav bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MovieList from "./components/MovieList.js";
+import Navbar from "./components/Navbar.js";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [query, setQuery] = useState("");
+
+  function handleQuery(q) {
+    console.log(q);
+    setQuery(q);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar onHandleQuery={handleQuery} />
+      <MovieList query={query} />
     </div>
   );
 }
-
-export default App;
